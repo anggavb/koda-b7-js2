63c7fc63e1919e700234cb9bb2a2c34279b67937
anggavb <anggavb8@gmail.com>
2026-02-25 14:43:14 +0700

    add switch statement

--- minitask-2.js
+++ minitask-2.js
@@ -1,7 +1,7 @@
-const purchase = 400000
+const purchase = 2000000
 let discount = 0
 
-if (purchase) {
+if (purchase && purchase > 0) {
   if (purchase >= 500000 && purchase <= 1000000) {
     discount = 0.05
   } else if (purchase > 1000000) {
@@ -14,3 +14,16 @@ if (purchase) {
 } else {
   console.log('Invalid purchase amount!')
 }
+
+switch (true) {
+  case (purchase >= 500000 && purchase <= 1000000):
+    discount = 0.05
+    break
+  case (purchase > 1000000):
+    discount = 0.1
+    break
+  default:
+    discount = 0
+}
+
+console.log(`${!purchase || purchase < 0 ? "Invalid purchase amount!" : `Anda mendapatkan diskon sebesar ${discount * 100}% dengan total diskon sebesar Rp${purchase * discount}`}`);
