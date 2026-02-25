84249e4aed5560d6bb9e29daee08ebb0ed7bf6e0
anggavb <anggavb8@gmail.com>
2026-02-25 14:21:51 +0700

    submit minitask 2 day 3

---
+++ minitask-2.js
@@ -0,0 +1,17 @@
+const purchase = 400000
+let discount = 0
+
+if (purchase) {
+  if (purchase >= 500000 && purchase <= 1000000) {
+    discount = 0.05
+  } else if (purchase > 1000000) {
+    discount = 0.1
+  } else {
+    discount = 0
+  }
+
+  console.log(`Anda mendapatkan diskon sebesar ${discount * 100}% dengan total diskon sebesar Rp${purchase * discount}`);
+  return;
+}
+
+console.log('Invalid purchase amount!')
